0b125646b544433c2aec5220de749aa57a9eb388
Himesh Parashar <himeshparashar424@gmail.com>
2024-12-24 20:23:29 +0530

    fix: redirect user to dashboard after authentication

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -10,7 +10,7 @@ type Props = {
 const Layout = async ({ children }: Props) => {
   const user = await currentUser();
 
-  if (user) redirect("/");
+  if (user) redirect("/dashboard");
 
   return (
     <div className="h-screen flex w-full justify-center">
